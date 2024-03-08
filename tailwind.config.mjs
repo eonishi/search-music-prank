19d184ca7dcc98b06763dec57bb7dec3ac6bcac2
Eduardo Onishi <eonishi@frba.utn.edu.ar>
2024-03-07 22:26:04 -0300

    Agrego configuracion de tailwind

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,24 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
-}
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      animation: {
+        "fade-in-down": "fade-in-down 0.6s ease-in-out",
+      },
+      "fade-in-down": {
+        "0%": {
+          opacity: "0",
+          transform: "translateY(-20px)",
+        },
+        "100%": {
+          opacity: "1",
+          transform: "translateY(0)",
+        },
+      },
+    },
+  },
+  plugins: [
+    require("@tailwindcss/typography")
+  ],
+};
