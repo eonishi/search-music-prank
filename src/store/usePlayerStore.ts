728c6d02d1a65642090d33f346e4ac1af896b5ca
Eduardo Onishi <eonishi@frba.utn.edu.ar>
2024-03-24 19:02:40 -0300

    Add volume state

--- src/store/usePlayerStore.ts
+++ src/store/usePlayerStore.ts
@@ -2,15 +2,17 @@ import { create } from "zustand";
 
 interface Song {
     tittle: string;
-    artist: string;
+    artists: string;
     cover: string;
 }
 
 const usePlayerStore = create((set) => ({
     isPlaying: false,
     currentSong: null as Song | null,
+    volume: 1,
     setIsPlaying: (isPlaying: Boolean) => set({ isPlaying }),
     setCurrentSong: (currentSong: Song) => set({ currentSong }),
+    setVolume: (volume: number) => set({ volume }),
 }));
 
 export default usePlayerStore;
