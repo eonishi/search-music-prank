25203cb366f17e16e013a7e29f67c0c94586f232
Eduardo Onishi <eonishi@frba.utn.edu.ar>
2024-03-24 19:04:00 -0300

    Add volume onChange events

--- src/components/PrankVideo.jsx
+++ src/components/PrankVideo.jsx
@@ -1,14 +1,16 @@
 import "./PrankVideo.css";
 import usePlayerStore from "../store/usePlayerStore.ts";
-import { useEffect, useRef, useState } from "react";
+import { lazy, useEffect, useRef} from "react";
 
 export default function PrankVideo() {
-  const { firstUpdate, setFirstUpdate } = useState(true);
-  const { isPlaying } = usePlayerStore((state) => state);
+  const { isPlaying, volume } = usePlayerStore((state) => state);
   const videoRef = useRef();
+  const audioRef = useRef();
+  const firstUpdate = useRef(true);
 
   useEffect(() => {
     isPlaying ? videoRef.current.play() : videoRef.current.pause();
+    isPlaying ? audioRef.current.play() : audioRef.current.pause();
   }, [isPlaying]);
 
   useEffect(() => {
@@ -17,26 +19,35 @@ export default function PrankVideo() {
       : videoRef.current.classList.remove("fadeIn");
   }, [isPlaying]);
 
-  
-  if (firstUpdate) {
-    setFirstUpdate(false);
-  } else {
-    useEffect(() => {
+  useEffect(() => {
+    if (firstUpdate.current) {
+      firstUpdate.current = false;
+    } else {
       isPlaying
         ? videoRef.current.classList.add("fadeIn")
         : videoRef.current.classList.add("fadeOut");
+    }
   }, [isPlaying]);
-  }
 
+  useEffect(() => {
+    audioRef.current.volume = volume;
+  }, [volume]);
 
   return (
     <>
       <video
         ref={videoRef}
+        autoPlay={false}
         src="/Gandalf.mp4"
         id="bg-video"
         className="bg-repeat-round group-active:animation"
+        muted={true}
       ></video>
+
+      <audio
+        src="/audio.mp3"
+        ref={audioRef}
+      ></audio>
     </>
   );
 }
