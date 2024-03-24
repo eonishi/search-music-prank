8730a368ffbc9fb479e406ebfc41bb9cd45a2585
Eduardo Onishi <eonishi@frba.utn.edu.ar>
2024-03-24 19:08:35 -0300

    Add TextAnimatedGrandient component

---
+++ src/components/TextAnimatedGradient.jsx
@@ -0,0 +1,11 @@
+const TextAnimatedGradient = (props) => {
+    return (
+      <span className='inline-flex justify-center animate-text-gradient 
+      bg-gradient-to-r from-[#bae6fd] via-[#f0f9ff] to-[#bae6fd] bg-[200%_auto] bg-clip-text
+      text-4xl text-transparent font-semibold '>
+        {props.children}
+      </span>
+    );
+  };
+  
+  export default TextAnimatedGradient;
