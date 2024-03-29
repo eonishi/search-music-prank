ed385ef3c2c980980419ce7e3be3ee2795d7a091
Eduardo Onishi <eonishi@frba.utn.edu.ar>
2024-03-29 16:12:34 -0300

    Add vercel dependencies

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,13 @@
 import { defineConfig } from 'astro/config';
 import htmx from "astro-htmx";
 import tailwind from "@astrojs/tailwind";
-
 import react from "@astrojs/react";
+
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
   output: 'server',
-  integrations: [tailwind(), htmx(), react()]
+  integrations: [tailwind(), htmx(), react()],
+  adapter: vercel()
 });
