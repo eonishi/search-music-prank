fb60923086b2c51f41546bee1ce1cd67ac2131d1
Eduardo Onishi <eonishi@frba.utn.edu.ar>
2024-03-29 11:51:00 -0300

    deleting some animations that are not being used.

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -4,38 +4,14 @@ export default {
   theme: {
     extend: {
       animation: {
-        "fade-in-down": "fade-in-down 0.6s ease-in-out",
         "text-gradient": "text-gradient 5s linear infinite",
-        "fade-in-up": "fade-in-up 0.6s ease-in-out",
       },
-      
-      "fade-in-up": {
-        "0%": {
-          "opacity": "0",
-          "transform": "translateY(20px)"
-        },
-        "100%": {
-          "opacity": "1",
-          "transform": "translateY(0)"
-        }
-      },
-
       "keyframes": {
         "text-gradient": {
           "to": {
             "backgroundPosition": "200% center"
           }
         }
-      },
-      "fade-in-down": {
-        "0%": {
-          opacity: "0",
-          transform: "translateY(-20px)",
-        },
-        "100%": {
-          opacity: "1",
-          transform: "translateY(0)",
-        },
       },
     },
   },
