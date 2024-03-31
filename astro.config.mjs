0d9262a83491c06f19380159b06d46a1b20c3cbf
Eduardo Onishi <eonishi@frba.utn.edu.ar>
2024-03-31 11:16:35 -0300

    Add netlify dependencies

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,15 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 import htmx from "astro-htmx";
 import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
-
 import vercel from "@astrojs/vercel/serverless";
+
+import netlify from "@astrojs/netlify";
 
 // https://astro.build/config
 export default defineConfig({
-  output: 'server',
+  output: "server",
   integrations: [tailwind(), htmx(), react()],
-  adapter: vercel()
+  //adapter: vercel()
+  adapter: netlify(),
 });
