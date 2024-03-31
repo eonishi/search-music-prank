7940027d6220bc8e4806ab9a3cdf8fe3cfb5ef32
Eduardo Onishi <eonishi@frba.utn.edu.ar>
2024-03-31 18:56:55 -0300

    Fix song-card animation

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,12 +4,9 @@ import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
 import vercel from "@astrojs/vercel/serverless";
 
-import netlify from "@astrojs/netlify";
-
 // https://astro.build/config
 export default defineConfig({
   output: "server",
   integrations: [tailwind(), htmx(), react()],
-  //adapter: vercel()
-  adapter: netlify(),
+  adapter: vercel(),
 });
--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -5,17 +5,27 @@ export default {
     extend: {
       animation: {
         "text-gradient": "text-gradient 5s linear infinite",
+        "fade-in-up": "0.6s ease-in-out 0s both fade-in-up",
       },
+
       "keyframes": {
         "text-gradient": {
           "to": {
-            "backgroundPosition": "200% center"
-          }
-        }
+            "backgroundPosition": "200% center",
+          },
+        },
+        "fade-in-up": {
+          "0%": {
+            "opacity": "0",
+            "transform": "translateY(20px)",
+          },
+          "100%": {
+            "opacity": "1",
+            "transform": "translateY(0)",
+          },
+        },
       },
     },
   },
-  plugins: [
-    require("@tailwindcss/typography")
-  ],
+  plugins: [require("@tailwindcss/typography")],
 };
